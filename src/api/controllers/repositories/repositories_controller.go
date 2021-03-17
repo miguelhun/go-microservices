3d3eb37648551a37579394275203723822098534
miguelhun <hunmiguel1124@gmail.com>
2021-03-16 23:58:21 -0500

    controller repo

---
+++ src/api/controllers/repositories/repositories_controller.go
@@ -0,0 +1,26 @@
+package repositories
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/miguelhun/go-microservices/src/api/domain/repositories"
+	"github.com/miguelhun/go-microservices/src/api/services"
+	"github.com/miguelhun/go-microservices/src/api/utils/errors"
+)
+
+func CreateRepo(c *gin.Context) {
+	var request repositories.CreateRepoRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		apiError := errors.NewBadRequestError("invalid json body")
+		c.JSON(apiError.Status(), apiError)
+		return
+	}
+
+	result, err := services.RepositoryService.CreateRepo(request)
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+	c.JSON(http.StatusCreated, &result)
+}
